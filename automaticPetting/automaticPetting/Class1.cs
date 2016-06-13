19cbae17cdbdf1471e4608eb4c94cc7653ceab68
FeHioe <fe.hioe@gmail.com>
2016-06-13 17:25:23 -0400

    automaticPetting v1
    mod works after sleeping one day, change trigger event to fix

---
+++ automaticPetting/automaticPetting/Class1.cs
@@ -0,0 +1,29 @@
+﻿using StardewModdingAPI;
+using StardewValley;
+using System;
+
+namespace automaticPetting
+{
+    public class animalHeartsClass : Mod
+    {
+        public override void Entry(params object[] objects)
+        {
+            Log.Info("automaticPetting has loaded");
+            StardewModdingAPI.Events.TimeEvents.DayOfMonthChanged += new_day; //Trigger event on each new day
+        }
+
+        static void new_day(object sender, EventArgs e)
+        {
+            Farm farm = Game1.getFarm();
+           
+            foreach (FarmAnimal animal in farm.getAllFarmAnimals())
+            {
+                if (!animal.wasPet)
+                {
+                    animal.wasPet = true;
+                }
+            }
+        }
+
+    }
+}
